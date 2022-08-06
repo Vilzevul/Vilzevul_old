e515bf7f630f5461c7812111a1cce766a11cef30
Vilzevul <sheggyshelti@mail.ru>
2022-08-06 21:58:45 +0700

    Initial commit

---
+++ src/test/java/telega_bot/Vilzevul/VilzevulApplicationTests.java
@@ -0,0 +1,13 @@
+package telega_bot.Vilzevul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VilzevulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
